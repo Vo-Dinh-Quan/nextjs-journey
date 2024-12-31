8f8027d7a9e09d1a96035d25dadfe8f1710fe191
Vo Dinh Quan <22521193@gm.uit.edu.vn>
2024-12-31 23:05:45 +0700

    15. Dark Light Mode với Shadcn UI Next.js

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <ModeToggle />
+      Login Page
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ client/src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <ModeToggle />
+      Register Page
+    </div>
+  )
+}
+
+export default RegisterPage
